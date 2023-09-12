4084c0437f1c9695fa31fc6e542d5fe66832d11b
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-09-12 10:44:53 -0400

    added setup.py with prelim options

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="cwipost",
+        version="0.1.0",
+        author="Ryan Hamilton",
+        author_email="ryan.hamilton@ec.gc.ca",
+        packages=find_packages(where="cwipost", include=["cwipost", "cwipost.*"]),
+    )

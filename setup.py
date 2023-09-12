4c8513d8c0e0a5424387abf8b216088f8349964a
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-09-12 10:53:40 -0400

    added entry points for cli

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
         author="Ryan Hamilton",
         author_email="ryan.hamilton@ec.gc.ca",
         packages=find_packages(where="cwipost", include=["cwipost", "cwipost.*"]),
+        entry_points={'console_scripts': [
+            'cwipost = cwipost.cli:cli'
+        ]}
+        
     )
